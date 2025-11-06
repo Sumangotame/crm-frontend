2907430720d94a0c229d5c3e7641ff6fa18bdd16
Sumangotame <iamsuperstarsuman@gmail.com>
2025-11-07 01:53:26 +0545

    environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: import.meta.env.REACT_APP_API_URL,
 })
 
 API.interceptors.request.use((config) => {
